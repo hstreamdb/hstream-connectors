f1544367c7152fb66af2a85c67ca935fe5aea633
s12f <97083380+s12f@users.noreply.github.com>
2024-01-11 10:58:48 +0800

    fix(java-toolkit): adjust client requestTimeoutMs and add reader.max.retry config (#100)

--- integration_tests/app/src/test/java/io/hstream/SourceGeneratorTest.java
+++ integration_tests/app/src/test/java/io/hstream/SourceGeneratorTest.java
@@ -23,31 +23,6 @@ public class SourceGeneratorTest {
 
   @SneakyThrows
   @Test
-  void testFullSync() {
-    var cfg =
-        mapper
-            .createObjectNode()
-            .put("stream", "stream01")
-            .put("type", "sequence")
-            .put("batchSize", 1)
-            .put("period", 1);
-    var connector =
-        helper.client.createConnector(
-            CreateConnectorRequest.newBuilder()
-                .name("src01")
-                .type(ConnectorType.valueOf("SOURCE"))
-                .target("generator")
-                .config(cfg.toString())
-                .build());
-    log.info("created connector:{}", connector);
-    Thread.sleep(5000);
-    var result = Utils.readStream(helper.client, "stream01", 10, 15);
-    var seq = result.size() - 1;
-    Assertions.assertEquals(seq, result.get(seq).getInt("id"));
-  }
-
-  @SneakyThrows
-  @Test
   void testJsonSchema() {
     var schema = "{\n" +
             "  \"type\": \"object\",\n" +
